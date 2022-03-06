483c0f044a133f8c3ca940b304a9df7ba8c8c083
abdes <abdes@users.noreply.github.com>
2022-03-06 16:27:37 +0000

    deploy: 36bc9bd65283f88a8988cf8da27ee39d71e876c9

--- asap_mixin_master/html/_static/documentation_options.js
+++ asap_mixin_master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0 (cd9f4f3ffed3)',
+    VERSION: '1.0.0 (36bc9bd65283)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
